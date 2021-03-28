62381a43f63dfe3f9dd4574a1b3a7e0cf234e517
Tyler Quinn <adminaccountrun@Tylers-MacBook-Pro-3.local>
2021-03-28 16:50:46 -0500

    "merge message"

---
+++ quinn_declaring.py
@@ -0,0 +1,15 @@
+
+
+music_genres = ["rock", "jazz", "electronic", "punk", "ambient"]
+
+print('')
+
+print(len(music_genres))
+
+print('')
+
+#Visiting every item in a list 
+
+for i in range(len(music_genres)):
+    print(music_genres[i])
+    print('')
